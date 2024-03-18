57aacbb043ce95b868ba19058c804ec7af648376
withLevi <zengyin.ssr@gmail.com>
2024-03-18 22:06:49 +0800

    modify backedn host.

--- src/app.tsx
+++ src/app.tsx
@@ -121,7 +121,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request = {
-  baseURL: 'http://localhost:8101',
+  baseURL: 'https://bi-backend.withlevi.top',
   withCredentials: true,  
   ...errorConfig,
 };
