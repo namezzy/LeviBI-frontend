350aacaf061b6885fe5763a8d6ac900669fd34fe
withLevi <zengyin.ssr@gmail.com>
2024-03-20 01:50:02 +0800

    fix baseURL

--- src/app.tsx
+++ src/app.tsx
@@ -121,8 +121,8 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request = {
-  // baseURL: 'https://bi-backend.withlevi.top',
-  baseURL: 'http://localhost:8101',
+   baseURL: 'https://bi-backend.withlevi.top',
+ // baseURL: 'http://localhost:8101',
   withCredentials: true,
   ...errorConfig,
 };
